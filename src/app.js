329615d3cb6870615a54ba86f39bfd856b280e96
Princewill Iroka <princewilliroka@outlook.com>
2021-05-14 15:24:53 +0100

    Initial app setup

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import hpp from 'hpp';
+import helmet from 'helmet';
+// import routes from './routes';
+
+const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(cors());
+app.use(hpp());
+app.use(helmet());
+// app.use('/api', routes);
+
+app.all('*', (req, res, next) => {
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, Content, Accept, Content-Type, Authorization, Content-Length, X-Requested-With'
+	);
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+
+app.get('/', (req, res) => {
+	res.status(200).json({
+		message: 'Welcome to TalentQlSecondStageAssessment!',
+	});
+});
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,53 @@
+import http from "http";
+import app from "./app";
+// import database from "./database";
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (typeof port !== "number") {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || 4000);
+
+app.set("port", port);
+
+const server = http.createServer(app);
+
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? `pipe ${address}` : `port: ${port}`;
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges.`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use.`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+server.on("error", errorHandler);
+server.on("listening", async () => {
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? `pipe ${address}` : `port: ${port}`;
+  // await database.connect();
+  console.log(`Listening on ${bind}`);
+});
+
+server.listen(port);
